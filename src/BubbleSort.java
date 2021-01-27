3c782558abb17d32bd1423aad2f7d361e5c17a8b
sky8604 <skyelar.m@urer.ch>
2021-01-27 21:48:56 +0100

    changed main class

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,6 +1,6 @@
 public class BubbleSort {
 
-    public int[] bubbleSort(int[] array) {
+    public int[] sort(int[] array) {
         int n = array.length;
         for (int i = 0; i < n - 1; i++)
             for (int j = 0; j < n - i - 1; j++)
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,15 +6,19 @@ public class Main {
 
     //time memory comparinsons loopos
 
-    private Map<String, Map<String, Float>> tenInts_measurements;
-    private Map<String, Map<String, Float>> hundredInts_measurements;
-    private Map<String, Map<String, Float>> thousandInts_measurements;
+    private static Map<String, Map<String, Long>> tenInts_measurements;
+    private static Map<String, Map<String, Long>> hundredInts_measurements;
+    private static Map<String, Map<String, Long>> thousandInts_measurements;
 
 
     public static void main(String[] args) {
         int[] tenInts = readFile("./ressources/10Digits.txt", new int[10]);
         int[] hundredInts = readFile("./ressources/100Digits.txt", new int[100]);
         int[] thousandInts = readFile("./ressources/1000Digits.txt", new int[1000]);
+
+        int[] tenSortedInts = getSortedIntsAndMeasurements(tenInts,10);
+        int[] hundredSortedInts = getSortedIntsAndMeasurements(hundredInts,100);
+        int[] thousandSortedInts = getSortedIntsAndMeasurements(thousandInts,1000);
 
     }
 
@@ -35,34 +38,61 @@ public class Main {
         return array;
     }
 
-    private static Map<String, int[]> sortTenInts(int[] array) {
-        Map<String, int[]> sorted_tenInt_arrays = new HashMap<>();
-
-        Map<String, Float> swap_measurements = new HashMap<>();
-        Map<String, Float> insertion_measurements = new HashMap<>();
-        Map<String, Float> merge_measurements = new HashMap<>();
-
+    private static int[] getSortedIntsAndMeasurements(int[] unsortedIntsArray, int howManyIntsInArray) {
         SwapSorter swapSort = new SwapSorter();
         Insertionsort insertionSort = new Insertionsort();
         MergeSort mergeSort = new MergeSort();
+        BubbleSort bubbleSort = new BubbleSort();
+        QuickSort quickSort = new QuickSort();
 
-        sorted_tenInt_arrays.put("swap", swapSort.sort(array));
-        sorted_tenInt_arrays.put("insert", insertionSort.sort(array));
-        //sorted_tenInt_arrays.put("merge", mergeSort.***)
+        int[] sortedInts = insertionSort.sort(unsortedIntsArray);
+        swapSort.sort(unsortedIntsArray);
+        //mergeSort.sort(unsortedIntsArray);
+        bubbleSort.sort(unsortedIntsArray);
+        quickSort.sort(unsortedIntsArray,1,100);
 
-        swap_measurements.put("time", swapSort.getTime);
-        swap_measurements.put("memory", swapSort.getMemory);
-        swap_measurements.put("comparisons", swapSort.getComparisons);
-        swap_measurements.put("loops", swapSort.getLoops);
+        putMeasurementsMapInMap(howManyIntsInArray,swapSort,insertionSort,mergeSort,bubbleSort,quickSort);
 
-        insertion_measurements.put("time", insertionSort.getTime);
-        insertion_measurements.put("memory", insertionSort.getMemory);
-        insertion_measurements.put("comparisons", insertionSort.getComparisons);
-        insertion_measurements.put("loops", insertionSort.getLoops());
+        return sortedInts;
+    }
 
+    private static void putMeasurementsMapInMap(int howManyIntsInArray, SwapSorter swapSort, Insertionsort insertionSort, MergeSort mergeSort, BubbleSort bubbleSort, QuickSort quickSort){
+        Map<String, Map<String, Long>> mapOfAllMeasurements = new HashMap<>();
 
+        Map<String, Long> swap_measurements = putMeasurementsInMap(swapSort.getTime(), swapSort.getMemory(), Long.valueOf(swapSort.getSwaps()),Long.valueOf(swapSort.getLoops()));;
+        Map<String, Long> insertion_measurements = putMeasurementsInMap(insertionSort.getTime,insertionSort.getMemory, insertionSort.getComparisons, insertionSort.getLoopPasses());
+        Map<String, Long> merge_measurements = putMeasurementsInMap(mergeSort.getTime, mergeSort.getMemory, mergeSort.getComparisons, mergeSort.getLoops);
+        Map<String, Long> bubble_measurements = putMeasurementsInMap(bubbleSort.getTime, bubbleSort.getMemory,bubbleSort.getComparisons, bubbleSort.getLoops);
+        Map<String, Long> quick_measurements = putMeasurementsInMap( quickSort.getTime,quickSort.getMemory,quickSort.getComparisons,quickSort.getLoops);
 
+        mapOfAllMeasurements.put("swap", swap_measurements);
+        mapOfAllMeasurements.put("insertion", insertion_measurements);
+        mapOfAllMeasurements.put("merge", merge_measurements);
+        mapOfAllMeasurements.put("bubble", bubble_measurements);
+        mapOfAllMeasurements.put("quick", quick_measurements);
 
+        switch (howManyIntsInArray) {
+            case 10:
+                tenInts_measurements = mapOfAllMeasurements;
+                break;
+            case 100:
+                hundredInts_measurements = mapOfAllMeasurements;
+                break;
+            case 1000:
+                thousandInts_measurements = mapOfAllMeasurements;
+                break;
+        }
+    }
+
+    private static Map<String, Long> putMeasurementsInMap(Long time, Long memory, Long comparisons, Long loops){
+        Map<String, Long> measurements = new HashMap<>();
+
+        measurements.put("time", time);
+        measurements.put("memory", memory);
+        measurements.put("comparisons", comparisons);
+        measurements.put("loops", loops);
+
+        return measurements;
     }
 }
 
